62417a1710321c5347e311a7219e58061c65d0b7
shark503 <76966259+shark503@users.noreply.github.com>
2024-06-18 13:31:17 -0600

    mostrar mas

--- static/js/main.js
+++ static/js/main.js
@@ -32,15 +32,27 @@ $(document).ready(function() {
                     errorMessage.text(response.error);
                 } else {
                     response.movies.forEach((movie, index) => {
+                        let shortText = response.overview[index].substring(0, 100);
+                        let remainingText = response.overview[index].substring(100);
                         let movieCard = `
-                            <div class="card" style="width: 18rem;">
-                                <img src="${response.posters[index]}" class="card-img-top" alt="${movie}">
-                                <div class="card-body">
-                                    <h2 class="card-title">${movie}</h2>
-                                    <p class='card-overview'>${response.overview[index]}</p>
+                            <div class="col-md-4">
+                                <div class="card mb-4">
+                                    <img src="${response.posters[index]}" class="card-img-top" alt="${movie}">
+                                    <div class="card-body">
+                                        <h5 class="card-title">${movie}</h5>
+                                        <p class="card-text text-justify">${shortText}<span class="dots">...</span><span class="moreText collapse" id="collapseOverview${index}">${remainingText}</span></p>
+                                        <a class="btn btn-link p-0 moreLessButton" data-toggle="collapse" href="#collapseOverview${index}" role="button" aria-expanded="false" aria-controls="collapseOverview${index}">Leer más</a>
+                                    </div>
                                 </div>
                             </div>`;
                         recommendations.append(movieCard);
+                    });
+
+                    // agrega el event listeners para los botones
+                    $('.moreLessButton').on('click', function () {
+                        let $this = $(this);
+                        let text = $this.text();
+                        $this.text(text === "Leer más" ? "Leer menos" : "Leer más");
                     });
                 }
             }
